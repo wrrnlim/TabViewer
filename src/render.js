d34edb3adadd1571e1a72c2fad8df078ffa11c4c
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2022-02-18 00:00:16 -0700

    #3 populate song list

--- src/render.js
+++ src/render.js
@@ -86,36 +86,69 @@ openFolderBtn.addEventListener('click', () => {
     ]
   }).then(file => {
     filePath = file.filePaths[0];
-    populateSongList();
+    const songList = indexFiles();
+    populateSongList(songList);
+    populateKeysList(songList);
+    populateChordsList(songList);
     selectDiv.classList.remove('hide');
     selectDiv.classList.add('tooltip');
     console.log(file);
   });
 });
 
-function populateSongList() {
+function indexFiles() {
   const files = fs.readdirSync(filePath);
-  selectSel.innerHTML = '<option value="Select song" selected disabled>Select song</option>';
+  
   let songList = {}
   files.forEach(file => {
-    file = file.split('.')[0];
-    const songName = file.split('-').slice(0,-2).join(' ');
-    const chordKey = file.split('-').slice(-2)[0];
-    const format = file.split('-').slice(-1)[0];
-    if (!(songName in songList)) {
-      let chords = {}
-      chords[chordKey] = [format];
-      songList[songName] = chords;
+    let filename = file.split('.')
+    if (filename.length > 1) { // continue if file not a folder
+      file = filename[0];
+      if (file.split('-').length >= 3) { // continue if file has correct naming
+        const songName = toTitleCase(file.split('-').slice(0,-2).join(' '));
+        const chordKey = file.split('-').slice(-2)[0];
+        const format = file.split('-').slice(-1)[0];
+        if (!(songName in songList)) {
+          let chords = {}
+          chords[chordKey] = [format];
+          songList[songName] = chords;
+        }
+        else if (!(chordKey in songList[songName])) {
+          songList[songName][chordKey] = [format];
+        }
+        else {
+          songList[songName][chordKey].push(format);
+        }
+      }
     }
-    else if (!(chordKey in songList[songName])) {
-      songList[songName][chordKey] = [format];
-    }
-    else {
-      songList[songName][chordKey].push(format);
-    }
-    // const option = document.createElement('option');
-    // option.innerHTML = file;
-    // selectSel.appendChild(option);
   });
+
+  return songList;
+}
+
+function populateKeysList(songList) {
+  selectSel.innerHTML = '<option value="Select song" selected disabled>Select song</option>';
   console.log(songList);
+  Object.keys(songList).forEach(song => {
+    const option = document.createElement('option');
+    option.innerHTML = song;
+    selectSel.appendChild(option);
+  });
+}
+
+function populateChordsList(songList) {
+  
+}
+
+function populateSongList(songList) {
+
+}
+
+function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
 }
